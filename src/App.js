1fefda5a6179ae5bd4d83bb8304665aecf830def
Kazushi Isogai <kazu150@gmail.com>
2021-03-08 23:57:02 +0900

    synchronize star count with refetch queries

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import client from './client';
 import { SEARCH_REPOSITORIES, ADD_STAR, REMOVE_STAR } from './graphql';
 
 const StarButton = props => {
-  const node = props.node;
+  const {node, query, first, last, before, after} = props;
   const totalCount = node.stargazers.totalCount;
   const starUnit = totalCount !== 1 ? "stars" : "star";
   const viewerHasStarred = node.viewerHasStarred;
@@ -21,7 +21,18 @@ const StarButton = props => {
   }
 
   return (
-    <Mutation mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}>
+    <Mutation mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
+      refetchQueries={ mutationResult => {
+        console.log({mutationResult})
+        return [
+          {
+            query: SEARCH_REPOSITORIES,
+            variables: { query, first, last, before, after }
+          }
+        ]
+      }
+      }
+    >
       {
         addOrRemoveStar => <StarStatus addOrRemoveStar={addOrRemoveStar} />
       }
@@ -92,7 +103,7 @@ class App extends Component {
                         <li key={node.id}>
                           <a href={node.url} target="_blank" rel="noreferrer">{node.name}</a>
                           &nbsp;
-                          <StarButton node={node} />
+                          <StarButton node={node} {...{query, first, last, before, after}} />
                         </li>
                       )
                     })}
